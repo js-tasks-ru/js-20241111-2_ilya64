85ac98648a4316b027d6384ec24f1d77d467deda
IliaOsipov <iljaosipov99@gmail.com>
2024-11-28 01:23:47 +0300

    выполнены задания для второго блока

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,33 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    return function(obj) { 
+        let propertiesArray = path.split('.');
 
+        return getterRecursive(obj, propertiesArray);
+    };
+}
+
+function getterRecursive(obj, propertiesArray) {
+    const property = propertiesArray.shift();
+    
+    if (propertiesArray.length > 0) {
+        if (Object.hasOwn(obj, property)) {
+            const subobj = obj[property];
+
+            return getterRecursive(subobj, propertiesArray);
+
+        }
+        else {
+            return;
+        }
+    }
+    else {
+        if (Object.hasOwn(obj, property)) {
+            return obj[property];
+        }
+        else {
+            return;
+        }
+    }
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,39 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+    let newString = '';
+    
+    if (size != undefined) {
+        if (string.length > 1) {
+            if (size != 0) {
+                newString += string[0];
+            }
+            
+            let amountOfRepeatedTimes = 1;
+    
+            for (let i = 1; i < string.length; ++i) {
+                if (string[i - 1] == string[i]) {
+                    amountOfRepeatedTimes++;
+                    
+                    if (amountOfRepeatedTimes <= size) {
+                        newString += string[i];
+                    }
+                }
+                else {
+                    amountOfRepeatedTimes = 1;
+                    
+                    if (size != 0) {
+                        newString += string[i];
+                    }
+                }
+            }
+        }
+    
+        return newString;
+    }
+    else {
+        newString = string;
+        
+        return newString;
+    }
 }
